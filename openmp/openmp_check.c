68502bfaefe159a9495573a5abb8a201072ab8a9
Paulus Lahur <paulus.lahur@csiro.au>
2020-09-22 18:48:19 +0800

    Changed the work into exchange of 2 arrays.

--- openmp/openmp_check.c
+++ openmp/openmp_check.c
@@ -18,6 +18,7 @@ int main(int argc, char** argv)
     #pragma omp parallel
     {
         maxThreads = omp_get_num_threads();
+        printf("maxThreads: %d \n", maxThreads);
     }
     // printf("maxThreads: %d \n", maxThreads);
 
@@ -51,8 +52,15 @@ int main(int argc, char** argv)
     }
     printf("nThreads: %d \n", nThreads);
 
-    int nElements = 100000 * maxThreads;
+    int nElements = 1000000 * maxThreads;
     double x[nElements];
+    double y[nElements];
+
+    for (int i = 0; i < nElements; ++i)
+    {
+        x[i] = (double)(i);
+        y[i] = (double)(i);
+    }
 
     // int NTHREADS = 1;
     omp_set_num_threads(nThreads);
@@ -68,14 +76,18 @@ int main(int argc, char** argv)
         int nElementsPerThread = nElements / nThreads;
         int lowerBound = tid * nElementsPerThread;
         int upperBound = lowerBound + nElementsPerThread;
+        double t;
         printf("Thread %d is working for elements: %d - %d ... \n", tid, lowerBound, upperBound);
         for (int i = lowerBound; i < upperBound; ++i)
         {
             // Some expensive process here
-            for (int j = 0; j < 200000; ++j)
+            for (int j = 0; j < 20000; ++j)
             {
                 // This computation is completely within the thread
-                x[i] = x[i] + 1.0;
+                //x[i] = x[i] + 1.0;
+                t = x[i];
+                x[i] = y[i];
+                y[i] = t;
             }
         }
         printf("Thread %d of %d ends \n", tid, nThreads);
